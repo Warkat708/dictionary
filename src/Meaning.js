1ae65eca2f64ec91a8fd15a557c645d35f83f195
Kat Alexakis <katherine.alexakis@gmail.com>
2021-05-03 14:21:18 +0100

    more styline

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react"; 
 import Synonyms from "./Synonyms";
+import "./Meaning.css"; 
 
 export default function Meaning (props) {
     return (
@@ -8,10 +9,12 @@ export default function Meaning (props) {
         {props.meaning.definitions.map(function(definition, index) {
             return(
                 <div key ={index}>
-                    <strong>Definition:</strong> {definition.definition}
-                    <br />
-                   <strong>Example:</strong>   <em>'{definition.example}'</em>
-                    <br />
+                <div className = "definition">
+                  <strong>{definition.definition}</strong> 
+                </div>
+                <div className = "example">
+                   <em>{definition.example}</em>
+                </ div>
                 <Synonyms synonyms = {definition.synonyms} />
                 </div>
             );
