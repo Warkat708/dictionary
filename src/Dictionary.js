8bd8e4cf4a5f6ed4c989f781c273cad24119ad66
Kat Alexakis <katherine.alexakis@gmail.com>
2021-05-03 14:01:20 +0100

    more styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,36 +4,50 @@ import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
+    let [keyword, setKeyword] = useState("wine");
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(null); 
 
     function handleResponse (response) {
-        console.log(response.data[0].phonetics[0].text);
-        console.log(response.data[0].meanings[0].definitions[0].definition);
         setResults(response.data[0]);
     
         }
 
-    function search(event) {
-        event.preventDefault(); 
-     
+    function search() { 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);  
-    
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault(); 
+        search();
     }
 
     function handleKeywordChange(event) {
         setKeyword (event.target.value); 
     }
 
+    function load() {
+        setLoaded(true);
+        search(); 
+    }
+
+    if (loaded) {
     return (
         <div className = "Dictionary">
             <section>
-        <form onSubmit = {search} >
+        <form onSubmit = {handleSubmit} >
             <input type = "search" autoFocus = {true} onChange={handleKeywordChange} placeholder= "Enter search term"></input>
         </form>
+        <div className = "hint">
+            Suggested words: sunset, wine, ubiquitous... 
+        </div>
         </section>
         <Results results = {results} />
         </div>
-    )
+    );
+} else {
+    load(); 
+    return "Loading"; 
+}
 }
